efd4031a9181748b50b5ed345ada832d28f65ecb
Majick Tek <kingtmw2@gmail.com>
2018-04-11 16:53:37 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include "MacSDL2.h"
 
-int main(int argc, char** argv) {
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
+//this is in c++ with C comments
+int main(int argc, char* argv[]) {
+    init();
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+  
+    window = SDL_CreateWindow(
+			    "hello_sdl2",
+			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			    SCREEN_WIDTH, SCREEN_HEIGHT,
+			    SDL_WINDOW_SHOWN
+			    );
+  if (window == NULL) {
+    //fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+    
+    return 1;
+  }
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(2000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
